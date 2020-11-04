cd2a795edf23892d71a135f0e08f5271a474935b
Amirali Askari <askariamirali99@gmail.com>
2020-11-04 14:45:32 -0800

    Finish this project.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 
 const Square = (props) => {
-  return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-  );
+      return (
+          <button
+              className="square"
+              onClick={props.onClick}
+          >
+              {props.value}
+          </button>
+      );
 }
 
 const Board = (props) => {
 
   const renderSquare = (i)  => {
-    return <Square />;
+      return <Square
+          value={props.squares[i]}
+          onClick={() => props.onClick(i)}
+      />;
   }
-
-  const status = 'Next player: X';
-
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -44,15 +46,76 @@ const Board = (props) => {
 
 
 const Game = (props) => {
-  
-  return (
+    const [history, setHistory] = useState([{squares: Array(9).fill(null),}
+    ]
+    );
+    const [isX, setIsX] = useState(true);
+
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+    const handleClick = (i) => {
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = isX ? 'X' : 'O';
+        setHistory(prevState => [...prevState, {squares: squares}]);
+        setIsX(prevState => !prevState);
+    }
+
+    const winner = calculateWinner(history[history.length - 1].squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (isX ? 'X' : 'O');
+    }
+
+    const jumpTo = (i) => {
+        setHistory(prevState => [...prevState.filter((step, index) => index <= i)]);
+        setIsX(i % 2 === 0)
+    }
+
+    const moves = history.map((step, move) => {
+        const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
+
+
+    const current = history[history.length - 1];
+    return (
     <div className="game">
         <div className="game-board">
-          <Board />
+          <Board onClick={(i) => handleClick(i)} squares={current.squares}/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
   );
