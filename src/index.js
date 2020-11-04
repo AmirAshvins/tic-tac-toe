58674b98d1c46c4c8c734a4f76358193fbc5f4df
Amirali Askari <askariamirali99@gmail.com>
2020-11-04 12:57:13 -0800

    Remove duplicates

--- src/index.js
+++ src/index.js
@@ -62,9 +62,9 @@ const Game = (props) => {
 
 ReactDOM.render(
   <React.StrictMode>
-  <Game />,
+  <Game />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
